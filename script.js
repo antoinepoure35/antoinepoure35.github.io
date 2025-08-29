9a31bcd0d46a6b7d4d328a7b7f499bbcf82b435e
antoinepoure35 <91958774+antoinepoure35@users.noreply.github.com>
2025-08-29 21:43:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Définir un tableau de classes (vous pouvez le remplacer par vos propres données)
 let classes = [
-    { name: "Classe A", students: ["Alice", "Bob"] },
-    { name: "Classe B", students: ["Charlie", "David"] },
+    { name: "6C", students: ["B.Louan", "B.Alice", "B.Oscar", "D.Edwin", "DF.Maxence", "D.Gaspard", "FP.Ethan", "G.Maëlia", "H.Eloïne", "H.Agathe", "H.Jules", "L.Paco", "L.Charles", "L.Clémence", "LC.Timothée", "L.Julia", "L.Louis", "M.Léon", "M.Naoufel", "M.Léa", "M.Amandine", "M.Manon", "N.Antonin", "PS.Maéna", "P.Yanis", "SQ.Mathéo", "T.Suzie", "V.Thomas"] },
+    { name: "6D", students: ["A.Louise", "A.Adem", "B.Lola", "B.Nael", "B.Camille", "B.Lucas", "C.Leo", "C.Jules", "D.Valentin", "F.Timothée", "F.Victoire", "G.Marie-Perrine", "G.Timéo", "HC.Mathias", "L.Augustin", "M.Antoine", "M.Faustine", "M.Baptiste", "NO.Marine-Eden", "P.Gabryel", "P.Raphaëlle", "R.Maina", "RT.Elisabeth", "S.Mathéo", "SF.Romann", "T.Enzo", "V.Paul", "V.Pauline"] },
+    { name: "4B", students: ["B.Clément", "B.Camille", "B.Coralie", "C.Edouard", "C.Corentin", "C.Pierre", "D.Alexis", "E.Simon", "G.Malo", "GT.Thomas", "G.Antone", "J.Roxane", "J.Laura", "J.Hugo", "LL.Juliette", "LQ.Bertille", "LR.Martin", "L.Adam", "M.Lucie", "MS.Evan", "M.Samuel", "MDF.Ombeline", "O.Natasha", "P.Elouan", "PP.Driss", "RM.Clementine", "R.Ziad", "R.Clémence", "R.Jeanne", "S.Louann"] },
+    { name: "3A", students: ["A.Antonin", "A.Héloïse", "B.Jade", "B.Adèle", "B.Yasmine", "B.Lise", "C.Maxence", "C.Robin", "C.Samuel", "C.Léandre", "DB.Diane", "D.Keziah", "D.Faustine", "D.Colin", "E.Martin", "F.Edouard", "G.Joan", "H.Antonin", "J.Maïwen", "J.Aaron", "LL.Joséphine", "L.Anton", "M.Charlie", "NG.Jocelin", "P.Adélaïde", "P.Anaïs", "Q.Nina", "R.Solenn", "T.Céleste", "T.Louis"] },
     // Ajoutez plus de classes ici...
 ];
 
